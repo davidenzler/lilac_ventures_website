e6a82fa2148114b6c87b4373588378fc2a259254
Justin Roome <justinroome@csus.edu>
2023-04-07 15:51:23 -0700

    adding file viewer

--- src/ClientPortal.tsx
+++ src/ClientPortal.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import ProgressBar from "./ProgressBar";
+import PdfViewer from "./PdfViewer";
 
 function ClientPortal() {
-  
-
   return (
     <div>
       <h1>Client Portal</h1>
       <br></br>
       <ProgressBar />
+      <PdfViewer />
     </div>
   );
 }
