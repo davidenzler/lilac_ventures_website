71cc0ab89976736b9d4d57c41de39796e8a47135
Nathaniel_Cruz <njcgameplayer@gmail.com>
2023-04-09 14:14:00 -0700

    Missed one edit in last commit

--- src/History.tsx
+++ src/History.tsx
@@ -4,6 +4,7 @@ function History(){
     return(
         <div>
             <h1>twest</h1>
+            <p>This will be a textbox that the client can put customized text Lilac Ventures, the client's company. Text on similiar websites usually include the following sections: History, Values, Mission, Etc. Filler text is added to demonstate the effects of styling. Lorem ipsum dolor sit amet. Et libero consequatur aut velit labore qui deleniti possimus eum atque quae At ducimus repudiandae qui consequatur omnis id rerum corrupti. Qui quaerat explicabo non tenetur quia rem sint quia. Sed tempora quod et vitae eius rem dolores quod sit officia praesentium.</p>
         </div>
     );
 }
