7932f0f6556b03868b70801d5970a658163bbed9
Nathaniel_Cruz <njcgameplayer@gmail.com>
2023-09-17 21:22:30 -0700

    Fixed bugs and tested API

--- api/controllers/appointmentController.ts
+++ api/controllers/appointmentController.ts
@@ -1,65 +1,65 @@
 const Appointment =require("../model/Appointment.ts");
 
-const addAppointment= async (res,req) => {
+const addAppointment= async (req,res) => {
  
     const {date,time,user,duration}=req.body;
     if(!date||!time||!user||!duration) return res.status(400).json({'message': 'Incomplete appointment. Please choose date, time, and appointment type.'});
     try{
-        const appointment = new Appointment(date,time,user,duration);
-        await appointment.save();
-        res.status(200).json({ message: 'Appointment added successfully!' });
+        const appointment = Appointment.create({
+            "date" : date,
+            "time" : time,
+            "user" : user,
+            "duration" : duration
+        })
+        res.sendStatus(200)
     }
     catch(err){
         res.status(500).json({ error: err.message });
     }
     
 }
-const delAppointment= async (res,req) => {
+const delAppointment= async (req,res) => {
     const {date,time} = req.params;
     if(!date||!time) return res.status(400).json({'message': 'Missing Search Date and Time'})
     try{
-        await Appointment.remove({date:date,time:time}).exec();
-        res.status(200).json({ message: 'Appointment deleted successfully!' });
+        await Appointment.deleteOne({date:date,time:time}).exec();
+        res.sendStatus(200)
     }
     catch(err){
         res.status(500).json({error:err.message});
     }
 }
-const editAppointment= async (res,req) => {
+const editAppointment= async (req,res) => {
     const{oldDate,oldTime}=req.params;
     const {date,time,user,duration}=req.body;
     if(!date||!time||!user||!duration) return res.status(400).json({'message': 'Incomplete appointment. Please choose date, time, and appointment type.'});
-    if(!oldDate||!oldTime) return res.status(400).json({'message': 'Missing Search Date and Time'})
     try{
-        await Appointment.remove({date:oldDate,time:oldTime});
-        const appointment = new Appointment(date,time,user,duration);
-        await appointment.save();
-        res.status(200).json({ message: 'Appointment edited successfully!' });
-    }catch(err){
-
-    }
-}
-const getAppointments= async (res,req) => {
-    const {user} = req.params;
-    if(!user) return res.status(400).json({'message': 'No user found'})
-    let appts=[];
-    try{
-        appts= await Appointment.find({user:user});
-        res.json(appts);
-        res.status(200).json({ message: 'Appointment retreived successfully!' });
-
+        await Appointment.updateOne({"date":oldDate,"time":oldTime},{"date":date,"time":time,"user":user,"duration":duration})
+        res.sendStatus(200)
     }
     catch(err){
         res.status(500).json({error:err.message});
     }
 }
-const getAllAppointments = async (res,req) => {
+const getAppointments= async (req,res) => {
+    const {user} = req.params;
+    if(!user) return res.status(400).json({'message': 'No user found'})
+    else{
+        let appts=[];
+        try{
+            appts= await Appointment.find({user:user});
+            res.json(appts);
+        }
+        catch(err){
+            res.status(500).json({error:err.message});
+        }
+    }
+}
+const getAllAppointments = async (req,res) => {
     let appts=[];
     try{
         appts= await Appointment.find({});
         res.json(appts);
-        res.status(200).json({ message: 'Appointments retreieved successfully!' });
-
     }
     catch(err){
         res.status(500).json({error:err.message});
--- api/routes/appointments.ts
+++ api/routes/appointments.ts
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController.ts');
-const verifyRoles = require('../middleware/verifyJWT.ts')
 
 router.post('/',appointmentController.addAppointment)
 
-router.get('/:user',appointmentController.getAllAppointments)
+router.get('/user/:user',appointmentController.getAppointments)
 
 router.get('/',appointmentController.getAllAppointments)
 
-router.post('/:date/:time',appointmentController.editAppointment)
+router.post('/edit/:date/:time',appointmentController.editAppointment)
 
-router.post('/:date/:time',appointmentController.delAppointment)
+router.post('/del/:date/:time',appointmentController.delAppointment)
 
 module.exports = router;
