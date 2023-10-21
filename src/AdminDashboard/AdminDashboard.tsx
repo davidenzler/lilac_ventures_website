6167f99f58b75cf44b95b148c707798f826baf46
pprabhu2727 <prasadprabhu@hotmail.com>
2023-10-21 15:01:59 -0700

    LIL-163,164,165,166 PDF Notification System

--- src/AdminDashboard/AdminDashboard.tsx
+++ src/AdminDashboard/AdminDashboard.tsx
@@ -1,44 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './adminDash.css';
-import {BsFiletypePdf, BsFillPersonFill} from 'react-icons/bs'
-import {BiDownArrow, BiUpArrow} from 'react-icons/bi'
-import { useEffect, useState } from 'react';
+import axios from '../api/axios';
+import {BsFiletypePdf, BsFillPersonFill} from 'react-icons/bs';
+import {BiDownArrow, BiUpArrow} from 'react-icons/bi';
 import { getUsersAtStep } from '../services/ProgressService';
-import { json } from 'node:stream/consumers';
-
-
-// need API call when someone uploads new doc and Admin has not seen
-// for each step -> gets all users. Checks users to see if anyone has docs not seen by admin
-// need clickable link to account
+import { getPDFUploads, deletePDFUpload } from '../services/pdfUploadsService';
 
 type Props = {
     step: number;
     isNewUpload: boolean;
 }
 
-function DropDownEntry(users: any) {
-    const entries = users['users'];
-    console.log(entries);
+type UploadStatus = {
+    [key: number]: boolean;
+};
+
+function DropDownEntry({users, pdfs}: {users: any, pdfs: string[]}) {
     return (
-        <ul>
-        {
-            entries.map( (entry: any) => {
-                return (
-                    <li className='row'>
-                        <p>{entry.email}</p>
-                        <p>{entry.firstname}</p>
-                        <p>{entry.lastName}</p>
-                        <p>Hello</p>
-                    </li>
+        <div>
+            <ul>
+            {
+                users.map((entry: any) => {
+                    return (
+                        <li className='row'>
+                            <p>{entry.email}</p>
+                            <p>{entry.firstname}</p>
+                            <p>{entry.lastName}</p>
+                            <p>Hello</p>
+                        </li>
+                    )
+                })
+            }
+            </ul>
+            {
+                pdfs.length > 0 && (
+                    <div className="newUploads">
+                        <h3>New Uploads:</h3>
+                        <ul>
+                        {
+                            pdfs.map(pdfName => (
+                                <li>
+                                    <a href={`http://localhost:8080/files/${pdfName}`} target="_blank" rel="noopener noreferrer" onClick={async () => {
+                                        const fileId = await getFileIdFromName(pdfName);
+                                        if (fileId) {
+                                            await setAdminViewedStatus(fileId);
+                                        }
+                                        await deletePDFUpload(pdfName);
+                                    }}>{pdfName}</a>
+                                </li>
+                            ))
+                        }
+                        </ul>
+                    </div>
                 )
-            })
-        }
-        </ul>
+            }
+        </div>
     );
 }
 
-
-function StepsDropDown(users: any) {
+function StepsDropDown({users, pdfs}: {users: any, pdfs: string[]}) {
     return (
         <div className='stepToggle'>
             <div className="row rowHeader">
@@ -47,7 +67,7 @@ function StepsDropDown(users: any) {
                 <h3>LastName</h3>
                 <h3>Something</h3>
             </div>
-            {users['users'].length !== 0 && <DropDownEntry users={users['users']} />}
+            {users.length !== 0 && <DropDownEntry users={users} pdfs={pdfs} />}
         </div>
     )
 }
@@ -55,17 +75,21 @@ function StepsDropDown(users: any) {
 const ToggleItem = ({step, isNewUpload}: Props) =>  {
     const [toggleThisElement, setToggleThisElement] = useState(false);
     const iconStyles = { display: "inline" };
-    const [users, setUsers] = useState({});
+    const [users, setUsers] = useState([]);
+    const [pdfs, setPdfs] = useState<string[]>([]);
     const [customer_count, setCustomer_count] = useState(0); 
 
     useEffect(() => {
-        const getUsers = async() => {
-            const response = await getUsersAtStep(step);
-            setUsers(response.users);
-            setCustomer_count(response.users.length);
+        const fetchData = async() => {
+            const userResponse = await getUsersAtStep(step);
+            setUsers(userResponse.users);
+            setCustomer_count(userResponse.users.length);
+            
+            const pdfUploads = await getPDFUploads(step.toString());
+            setPdfs(pdfUploads.map((upload: any) => upload.pdf_name)); 
         }
 
-        getUsers();
+        fetchData();
     }, [step]);
 
     return (
@@ -78,22 +102,52 @@ const ToggleItem = ({step, isNewUpload}: Props) =>  {
                     <button onClick={() => setToggleThisElement((prev) => !prev)}>{toggleThisElement ? <BiUpArrow style={iconStyles}/> : <BiDownArrow style={iconStyles}/>}</button>
                 </div>
             </div>
-            {toggleThisElement && <StepsDropDown users={users} />}
+            {toggleThisElement && <StepsDropDown users={users} pdfs={pdfs} />}
         </>
     );
 }
 
+const getFileIdFromName = async (filename: string) => {
+    try {
+        const response = await axios.get(`files/getFileId/${filename}`);
+        return response.data.fileId; 
+    } catch (error) {
+        console.error(`Error fetching fileId for filename ${filename}:`, error);
+        return null;
+    }
+};
+
+const setAdminViewedStatus = async (fileId: string) => {
+    try {
+        await axios.put(`files/pdfModel/${fileId}/true`);
+    } catch (error) {
+        console.error(`Error setting seenByAdmin for fileId ${fileId}:`, error);
+    }
+};
+
 export default function AdminOverview() {
-    const isNewUpload = true;
+    const [stepsUploadStatus, setStepsUploadStatus] = useState<UploadStatus>({});
     const stepsSuccess = [1, 2, 3, 4, 5, 6, 7];
+
+    useEffect(() => {
+        const fetchNewUploads = async () => {
+            const status: any = {};
+            for (const step of stepsSuccess) {
+                const uploads = await getPDFUploads(step.toString());
+                status[step] = uploads && uploads.length > 0; 
+            }
+            setStepsUploadStatus(status);
+        }
+        fetchNewUploads();
+    }, []);
 
     return (
         <div className='overview'>
         {
             stepsSuccess.map((step) => {
-                return <ToggleItem step={step} isNewUpload={isNewUpload} />;
+                return <ToggleItem step={step} isNewUpload={stepsUploadStatus[step] || false} />;
             })
         }
         </div>
-    )
+    );
 }
