937d10f33be4e178f20eb10827df800530e59b35
favalos22 <127374446+favalos22@users.noreply.github.com>
2023-11-21 19:02:58 -0800

    LIL-198 (#107)

--- src/services/RegisterService.tsx
+++ src/services/RegisterService.tsx
@@ -1,25 +1,15 @@
 import axios from 'axios';
 
-export async function register(firstName: string, lastName: string, 
-    email: string, phone: number, street: string, city: string, state: string,
-    zip: number, marital: string, employment: string, cPreference: string) {
+export async function register(email: string, password: string) {
         try{
             const response = await axios.post('http://127.0.0.1:8080/register', {
-                firstName, 
-                lastName,
-                email,
-                phone, 
-                street,
-                city, 
-                state, 
-                zip,
-                marital,
-                employment, 
-                cPreference
+               email,
+               password
+
             });
             return response.data;
         } catch (error){
             throw error;
         }
-    }
+}
 
