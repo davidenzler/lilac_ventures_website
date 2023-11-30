5b58ae19dbd108502790f9a8da1252b9cf24edb8
davidenzler <73456941+davidenzler@users.noreply.github.com>
2023-11-30 00:57:25 -0800

    fixed pdf download from progress page

--- src/progress-bar-components/steps/Step3.tsx
+++ src/progress-bar-components/steps/Step3.tsx
@@ -60,7 +60,7 @@ const Form: React.FC<FormProps> = ({ name, onUploadSuccess, currentUser }) => {
     return (
         <div style={{ display: "flex", marginBottom: "10px" }}>
             <div style={{ marginRight: '20px' }}>Upload your file for <a
-                href={`${process.env.REACT_APP_API_URL}/files/${name}.pdf`} // Add ".pdf" to the URL
+                href={encodeURI(`${process.env.REACT_APP_API_URL}/files/${name}.pdf`)} // Add ".pdf" to the URL
                 target="_blank"
                 rel="noreferrer"
                 style={{ color: "#9b5bd4", textDecoration: 'underline' }}
